618cf28b6954991fb286729c20526e83680ebde9
Tim <ridenowhere90@gmail.com>
2021-08-01 11:57:05 +0900

    Hikari 설정중

--- spring-data-example/src/main/java/org/example/springdata/config/DataSourceConfig.java
+++ spring-data-example/src/main/java/org/example/springdata/config/DataSourceConfig.java
@@ -3,6 +3,7 @@ package org.example.springdata.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.embedded.DataSourceFactory;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
@@ -13,13 +14,14 @@ import javax.sql.DataSource;
 public class DataSourceConfig {
 
     @Bean
-    public DataSource dataSource() {
+    public DataSource dataSource(DataSourceFactory dataSourceFactory) {
         return new EmbeddedDatabaseBuilder()
                 .generateUniqueName(true)
                 .setType(EmbeddedDatabaseType.H2)
                 .setScriptEncoding("UTF-8")
                 .ignoreFailedDrops(true)
-                .addScript("classpath:/database/schema.sql")
+                //.addScript("classpath:/database/h2/schema.sql")
+                .setDataSourceFactory(dataSourceFactory)
                 .build();
     }
 
