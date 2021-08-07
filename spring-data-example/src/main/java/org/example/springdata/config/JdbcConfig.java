f077ad256fec1affb0d80c8aa982f73ed3b98085
Tim <iwasahorse@gmail.com>
2021-08-07 11:12:33 +0900

    JdbcConfig 수정(JdbcOperations 빈 제거)

--- spring-data-example/src/main/java/org/example/springdata/config/JdbcConfig.java
+++ spring-data-example/src/main/java/org/example/springdata/config/JdbcConfig.java
@@ -3,10 +3,6 @@ package org.example.springdata.config;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
-import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
-import org.springframework.jdbc.core.JdbcOperations;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
@@ -21,12 +17,6 @@ public class JdbcConfig  {
     public TransactionManager transactionManager(@Qualifier("dataSource") DataSource dataSource) {
         return new DataSourceTransactionManager(dataSource);
     }
-
-    @Bean
-    public JdbcOperations jdbcOperations(DataSource dataSource) {
-        return new JdbcTemplate(dataSource);
-    }
-
     @Bean
     public NamedParameterJdbcOperations namedParameterJdbcOperations(DataSource dataSource) {
         return new NamedParameterJdbcTemplate(dataSource);
