c05284dafdf2d46d90575a650a7e97253bc6031b
Tim <iwasahorse@gmail.com>
2021-08-14 17:58:40 +0900

    서블릿 필터 등록(CharacterEncodingFilter)

--- spring-webmvc-example/src/main/java/org/example/webmvc/config/MyWebAppInitializer.java
+++ spring-webmvc-example/src/main/java/org/example/webmvc/config/MyWebAppInitializer.java
@@ -1,6 +1,10 @@
 package org.example.webmvc.config;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+import java.nio.charset.StandardCharsets;
 
 public class MyWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
@@ -17,5 +21,13 @@ public class MyWebAppInitializer extends AbstractAnnotationConfigDispatcherServl
     @Override
     protected String[] getServletMappings() {
         return new String[] { "/" };
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding(StandardCharsets.UTF_8.toString());
+        return new Filter[] {
+                characterEncodingFilter };
     }
 }
